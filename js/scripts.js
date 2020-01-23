dfa408c9885e1447d06a272f878cd81bfa523027
Hoang Pham <tienhoang.pham@bc.fi>
2020-01-23 09:33:20 +0200

    update scripts with code comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-var registeredUsers = [];
+var registeredUsers = []; // this array stores valid usernames until the next pageload
 
 function validateForm(e){
-    e.preventDefault();
+    e.preventDefault(); // stop the submit button from refreshing the page
     console.log('validating....');
 
     console.log('user name: ' + validateUsername());
@@ -24,12 +24,20 @@ function renderRegisteredUsers() {
     });
 }
 
+/**
+ * this function supposely validates submitted username
+ * @returns [Boolean] true when valid, false otherwise
+ */
 function validateUsername(){
     var _userName = getUserName();
     
     return !checkSpace(_userName);
 }
 
+/**
+ * this function supposely validates submitted email
+ * @returns [Boolean] true when valid, false otherwise
+ */
 function validateEmail(){
     var _email = getEmail();
 
@@ -43,6 +51,7 @@ function validateEmail(){
         return false;
     }
 
+    // check if there is a dot located less than 2 symbols away from the @ sign
     var dot = _email.indexOf('.');
     if(dot <= atSymbol + 2) {
         return false;
@@ -56,6 +65,12 @@ function validateEmail(){
     return true;
 }
 
+/**
+ * this function supposely validates submitted password
+ * if password and confirmPassword do not match, return false 
+ * 
+ * @returns [Boolean] true when valid, false otherwise
+ */
 function validatePassword() {
     var _password = getPassword();
     var _confirmPassword = getConfirmPassword();
@@ -67,10 +82,23 @@ function validatePassword() {
     return true;
 }
 
+/**
+ * this function supposely checks whether the sample is an empty string
+ * or there is space within it
+ * @param [String] sample text to be evaluated
+ * @returns [Boolean] true when valid, false otherwise
+ */
 function checkSpace(sample) {
     return sample === '' || sample.indexOf(' ') > -1;
 }
 
+/**
+ * this function looks under the form with name "registration"
+ * look under the "username" input field and returns the value of it
+ * returns nothing if no value is found
+ * 
+ * @returns [Boolean] true when valid, false otherwise
+ */
 function getUserName() {
     if (typeof(document.registration.username.value) === 'undefined') {
         return '';
