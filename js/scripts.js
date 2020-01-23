e345da9e5ffeea1bf6888cb68b6f6821870532c1
Hoang Pham <tienhoang.pham@bc.fi>
2020-01-23 23:43:53 +0200

    add solution codes

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,46 @@ var registeredUsers = []; // this array stores valid usernames until the next pa
 
 function validateForm(e){
     e.preventDefault(); // stop the submit button from refreshing the page
-    console.log('validating....');
+    console.log('validating ....');
 
     console.log('user name: ' + validateUsername());
     console.log('email: ' + validateEmail());
     console.log('password: ' + validatePassword());
 
-    if (validateUsername() && validateEmail() && validatePassword()) {
-        var _newUser = getUserName();   
-        // add code to update registeredUsers array with new user and call render function
+    if (validateUsername() 
+            && validateEmail() 
+            && validatePassword() 
+            && validatePhoneNumber()
+            && validateFirstName()
+            && validateLastName()
+        ) {
+        var _newUser = getUserDataObj();   
+        // add code to update registeredUsers array with new username and call render function
         // TODO
+        registeredUsers.push(_newUser);
+        renderRegisteredUsers();
         document.registration.reset(); // reset form input fields
     }
 }
 
+function getUserDataObj() {
+    return {
+        userName: getUserName(),
+        firstName: getFirstName(),
+        lastName: getLastName(),
+        email: getEmail(),
+        phoneNumber: getPhoneNumber(),
+        password: getPassword(),
+        confirmPassword: getConfirmPassword()
+    };
+}
+
 function renderRegisteredUsers() {
+    document.getElementById('registered-users').innerHTML = '';
+
     registeredUsers.forEach(function(registeredUser){
         var _newUser = document.createElement('li'); 
-        _newUser.innerHTML = registeredUser;
+        _newUser.innerHTML = JSON.stringify(registeredUser);
         document.getElementById('registered-users').appendChild(_newUser);
     });
 }
@@ -32,6 +54,36 @@ function validateUsername(){
     var _userName = getUserName();
     
     return !checkSpace(_userName);
+}
+
+/**
+ * this function supposely validates submitted username
+ * @returns [Boolean] true when valid, false otherwise
+ */
+function validateFirstName(){
+    var _firstName = getFirstName();
+    
+    return (_firstName !== '');
+}
+
+/**
+ * this function supposely validates submitted username
+ * @returns [Boolean] true when valid, false otherwise
+ */
+function validateLastName(){
+    var _lastName = getLastName();
+    
+    return (_lastName !== '');
+}
+
+/**
+ * this function supposely validates submitted username
+ * @returns [Boolean] true when valid, false otherwise
+ */
+function validatePhoneNumber(){
+    var _phoneNumber = getPhoneNumber();
+    
+    return (!isNaN(_phoneNumber));
 }
 
 /**
@@ -79,6 +131,10 @@ function validatePassword() {
         return false;
     }
 
+    if (_password.length < 8) {
+        return false;
+    }
+
     return true;
 }
 
@@ -107,14 +163,50 @@ function getUserName() {
     }   
 }
 
+function getFirstName() {
+    if (typeof(document.registration.firstname.value) === 'undefined') {
+        return '';
+    } else {
+        return document.registration.firstname.value;
+    }   
+}
+
+function getLastName() {
+    if (typeof(document.registration.lastname.value) === 'undefined') {
+        return '';
+    } else {
+        return document.registration.lastname.value;
+    }   
+}
+
+function getPhoneNumber() {
+    if (typeof(document.registration.phonenumber.value) === 'undefined') {
+        return '';
+    } else {
+        return document.registration.phonenumber.value;
+    }   
+}
+
 function getEmail() {
-    // TODO
+    if (typeof(document.registration.email.value) === 'undefined') {
+        return '';
+    } else {
+        return document.registration.email.value;
+    }   
 }
 
 function getPassword() {
-    // TODO
+    if (typeof(document.registration.password.value) === 'undefined') {
+        return '';
+    } else {
+        return document.registration.password.value;
+    }   
 }
 
 function getConfirmPassword() {
-    // TODO
+    if (typeof(document.registration.password_confirm.value) === 'undefined') {
+        return '';
+    } else {
+        return document.registration.password_confirm.value;
+    }   
 }
