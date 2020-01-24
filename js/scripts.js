8f2862b9bd6065d0aa56ef84d4611e066a581b48
Hoang Pham <tienhoang.pham@bc.fi>
2020-01-24 09:38:34 +0200

    add exercise files

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,29 @@
-console.log('That was it');
+function calculate(order) {
+    var result;
+    var firstInput = parseInt(document.getElementById('firstinput').value);
+    var secondInput = parseInt(document.getElementById('secondinput').value);
 
+    if (order === 'add') {
+        result = add(firstInput, secondInput);
+    } else if (order === 'subtract') {
+        result = subtract(firstInput, secondInput);
+    }
 
+    document.getElementById('final-result').innerText = result;
+}
 
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b) {
+
+}
+
+function multiply(a, b) {
+
+}
+
+function divide(a, b) {
+
+}
