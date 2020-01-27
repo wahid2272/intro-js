5903e9cc847b2739791612fba9e9301011b5f4c7
Hoang Pham <tienhoang.pham@bc.fi>
2020-01-27 10:16:42 +0200

    Add image-slider script

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
-console.log('That was it');
+(function () {
+    var itemClassName = 'slider__photo';
+        items = document.getElementsByClassName(itemClassName),
+        totalItems = items.length,
+        slide = 0;
 
+    // Set event listeners
+    function setEventListeners() {
+        var next = document.getElementsByClassName('slider__button--next')[0],
+            prev = document.getElementsByClassName('slider__button--prev')[0];
 
+        next.addEventListener('click', getNext);
+        prev.addEventListener('click', getPrev);
+    }
 
+    function getNext() {
+        // TODO
+    }
+
+    function getPrev(){
+        // TODO      
+    }
+
+    function initSlider() {
+        // setInitialClasses();
+        setEventListeners();
+        
+        // Set moving to false so that the slider becomes interactive
+        moving = false;
+    }
+
+    initSlider();
+})();
